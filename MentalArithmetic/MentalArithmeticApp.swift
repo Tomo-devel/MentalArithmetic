f16a88cc1336f54f08ee8b6c79548daac324cc02
Tomo-dev <124762208+tomo-devl@users.noreply.github.com>
2023-07-08 16:26:53 +0900

    Initial Commit

---
+++ MentalArithmetic/MentalArithmeticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalArithmeticApp.swift
+//  MentalArithmetic
+//
+//  Created by 井上智輝 on 2023-07-08.
+//
+
+import SwiftUI
+
+@main
+struct MentalArithmeticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
